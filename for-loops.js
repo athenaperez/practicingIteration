c9f93dc38a1c43550b4457e9e80aa83a8679da14
Athena Perez <athenaperez@me.com>
2016-12-04 15:29:09 -0600

    added basic css, added first two for loop requirments

---
+++ for-loops.js
@@ -0,0 +1,30 @@
+
+// var counter = 0;
+// while ( counter <= 10 ) {
+//   console.log(counter);
+//   document.write( counter + ' ' );
+//   counter += 1;
+// }
+
+// for ( var counter = 0; counter <= 10; counter +=1 ) {
+// 	document.write( counter + '  ' );
+// 	// console.log( counter )
+// }
+
+
+
+// INCREMENT BY 10
+
+for (var counter = 5; counter <= 120; counter += 10) {
+   console.log( counter )
+}
+
+
+// DECREMENT BY DIVISION
+
+for (var counter = 4096; counter >= 1; counter /= 2) {
+   console.log( counter )
+}
+
+// ARRAY ITERATION
+
