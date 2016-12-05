0521c5c0321fca7e4fd9a18d4f85de02bd3d83e0
Athena Perez <athenaperez@me.com>
2016-12-04 21:29:41 -0600

    started american president section of JS

--- for-loops.js
+++ for-loops.js
@@ -15,16 +15,62 @@
 
 // INCREMENT BY 10
 
-for (var counter = 5; counter <= 120; counter += 10) {
-   console.log( counter )
-}
+// for (var counter = 5; counter <= 120; counter += 10) {
+//    console.log( counter )
+// }
 
 
 // DECREMENT BY DIVISION
 
-for (var counter = 4096; counter >= 1; counter /= 2) {
-   console.log( counter )
-}
+// for (var counter = 4096; counter >= 1; counter /= 2) {
+//    console.log( counter )
+// }
 
 // ARRAY ITERATION
+
+// var shoppingList = [
+// 'carrots',
+// 'milk',
+// 'eggs'
+// ];
+// alert(shoppingList);
+// console.log( shoppingList[0] );
+// console.log(shoppingList[2] );
+
+var americanPresidents = [
+	'George Washington',
+	'James Adams',
+	'Thomas Jefferson',
+	'George Bush',
+	'Bill Clinton',
+	'Jimmy Carter',
+	'John Kennedy'
+];
+
+for (var i = 0; i < americanPresidents.length; i += 1) {
+	console.log(americanPresidents[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
